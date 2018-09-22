6e223f83c371b2c8485bdbbcea80f5d949b95365
OlegUrmanov <olegurmanov@mail.ru>
2018-09-22 11:41:17 +0300

    Fixed script

--- Task1/js/registration-form.js
+++ Task1/js/registration-form.js
@@ -1,18 +1,18 @@
-function checkEmail(str) {
-  var result = str.match(/^[0-9a-z-\.]+\@[0-9a-z-]{1,}\.[a-z]{2,}$/i);
-  if (!result) {
+function checkEmail(str){//qwe212@mmal.ew
+  var result = str.match(/^[0-9-a-z-\.]+\@[0-9-a-z]{1,}\.[a-z]{2,}$/i);
+  if (!result){
     alert('Введите валидный e-mail.');
     return false;
   }
   return true;
 }
 
-function checkPassword(pass, pass_again) {
+function checkPassword(pass, confrim) {
   if (pass.length < 6) {
     alert('Длина пароля должна быть не менее 6-ти символов.');
     return false;
   }
-  if (pass != pass_again) {
+  if (pass != confrim) {
     alert('Введенные пароли не совпадают.');
     return false;
   }
@@ -31,14 +31,14 @@ function checkAgreementRules() {
 function registration() {
   var email = document.getElementsByClassName('registration__login')[0].value;
   var pass = document.getElementsByClassName('password-block__password')[0].value;
-  var pass_again = document.getElementsByClassName('password-block__password-repeat')[0].value;
-  if (checkEmail(email) && checkPassword(pass, pass_again) && checkAgreementRules()) {
+  var passConfirm = document.getElementsByClassName('password-block__password-repeat')[0].value;
+  if (checkEmail(email) && checkPassword(pass, passConfirm) && checkAgreementRules()) {
     alert('Регистрация прошла успешно!');
     return true;
-   }
-   return false;
- }
-
-document.getElementsByClassName('registration')[0].onsubmit = function() {
-  registration();
+  }
+  return false;
 }
+
+document.getElementsByClassName('registration')[0].onsubmit = function(){
+  return registration();
+};
