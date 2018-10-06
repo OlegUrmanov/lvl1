7bf2f4c2f3d971dfae23fdeb4837e08844f89cdc
OlegUrmanov <olegurmanov@mail.ru>
2018-10-06 10:06:34 +0300

    Add function Unsubscribe All

--- Task4/group-subscribe.js
+++ Task4/group-subscribe.js
@@ -1,12 +1,6 @@
 const myGroups = []; // массив для хранения подписок на группы
 
-const groupList = [{
-    groupName: "Habrahabr"
-  },
-  {
-    groupName: "Web2018"
-  },
-];
+const groupList = [{groupName: "Habrahabr"},{groupName: "Web2018"}];
 
 /**
  * Функция подписки на группы
@@ -14,13 +8,16 @@ const groupList = [{
  */
 function subscribeGroup(group) {
   for (let key of groupList.values()) {
-    if (key.groupName == group && myGroups.indexOf(group) == -1) {
+    if (/*key.groupName == group &&*/ myGroups.indexOf(group) == -1) {
       myGroups.push(group);
       console.log("you are just subscribe on " + group);
       return 0;
+    } else
+    if (key.groupName == group) {
+      console.log("can't do it. You already subscribe on " + group);
     }
-  }
-  console.log("can't subscribe on " + group + " group");
+}
+  console.log("can't subscribe on group " + group);
 }
 
 /**
@@ -32,23 +29,27 @@ function unsubscribeGroup(group) {
     myGroups.splice(myGroups.indexOf(group), 1);
     console.log("you are just unsubscribe for " + group);
   } else
+  if (myGroups.indexOf(group) != -2 ){
+    var pos = 0, n = 2;
+    var All = myGroups.splice (pos, n);
+    console.log("you are just unsubscribe for All")
+  } else
     console.log("can't unsubscribe on " + group);
 }
 
 function test_() {
   console.log("\nfunction subscribeGroup(" + "Habrahabr" + ")");
   console.log(subscribeGroup("Habrahabr"));
-  console.log("\nfunction subscribeGroup(" + "Web2017" + ")");
-  console.log(subscribeGroup("Web2017"));
-  console.log("\nfunction subscribeGroup(" + "Web2018" + ")");
-  console.log(subscribeGroup("Web2018"));
   console.log("\nfunction subscribeGroup(" + "Web2018" + ")");
   console.log(subscribeGroup("Web2018"));
 
-  console.log("\nfunction unsubscribeGroup(" + "Web2017" + ")");
-  console.log(unsubscribeGroup("Web2017"));
+  console.log("\nfunction unsubscribeGroup(" + "Web2018" + ")");
+  console.log(unsubscribeGroup("Web2018"));
   console.log(myGroups);
   console.log("\nfunction unsubscribeGroup(" + "Habrahabr" + ")");
   console.log(unsubscribeGroup("Habrahabr"));
+  console.log(myGroups);
+  console.log("\nfunction unsubscribeGroup(" + "All" + ")");
+  console.log(unsubscribeGroup(All));
   console.log(myGroups);
 }
