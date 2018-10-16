50cc349eae5ab70b7872f15ef4ff36f45a199aac
OlegUrmanov <olegurmanov@mail.ru>
2018-10-16 19:34:39 +0300

    Fixed struct_bat.

--- Task 3/struct_bat.bat
+++ Task 3/struct_bat.bat
@@ -1,39 +1,24 @@
-set /p x="BBeduTe umia papki: "
 @Echo Off
-MD C:\%x%
-MD C:\%x%\css
-MD C:\%x%\js
-echo Create style.css
-echo. html {>C:\%x%\css\style.css
-echo. >>C:\%x%\css\style.css
-echo. }>>C:\%x%\css\style.css
-echo. >>C:\%x%\css\style.css
-echo. body {>>C:\%x%\css\style.css
-echo. >>C:\%x%\css\style.css
-echo. }>>C:\%x%\css\style.css
-echo. >>C:\%x%\css\style.css
-echo. .content {>>C:\%x%\css\style.css
-echo. >>C:\%x%\css\style.css
-echo. }>>C:\%x%\css\style.css
-echo. >>C:\%x%\css\style.css
-echo. .content h1 {>>C:\%x%\css\style.css
-echo. >>C:\%x%\css\style.css
-echo. }>>C:\%x%\css\style.css
-echo Create app.js
-echo. >C:\%x%\js\app.js
-echo Create index.html
-echo. ^<^!DOCTYPE html^>^ >C:\%x%\index.html
-echo. ^<^html lang="en"^>^ >>C:\%x%\index.html
-echo. ^<^head^>^ >>C:\%x%\index.html
-echo.   ^<^meta charset="UTF-8"^>^ >>C:\%x%\index.html
-echo.   ^<^title>Title</title^>^ >>C:\%x%\index.html
-echo.   ^<^link rel="stylesheet" href="css/style.css"/^>^ >>C:\%x%\index.html
-echo. ^<^/head^>^ >>C:\%x%\index.html
-echo. ^<^body^>^ >>C:\%x%\index.html
-echo.   ^<^div class="content"^>^ >>C:\%x%\index.html
-echo.     ^<^h1^>^Hello! I'm ready for edit! :)^<^/h1^>^ >>C:\%x%\index.html
-echo.   ^<^/div^>^ >>C:\%x%\index.html
-echo.   ^<^script src="js/app.js"^>^^<^/script^>^ >>C:\%x%\index.html
-echo. ^<^/body^>^ >>C:\%x%\index.html
-echo. ^<^/html^>^ >>C:\%x%\index.html
+SetLocal EnableDelayedExpansion
+
+set /p Folder_name="Enter the name of the folder: "
+Set folder=%Folder_name%
+Set "Empty_str="
+If Exist "%folder%\*.*" (
+Echo The project is already created!
+) Else (
+MD %Folder_name%
+MD %Folder_name%\css
+for /f "UseBackQ Delims=" %%R IN ("css_struct.txt") do (
+echo. %%R>>%Folder_name%\css\style.css
+echo. %Empty_str%>>%Folder_name%\css\style.css
+)
+MD %Folder_name%\js
+echo. >%Folder_name%\js\app.js
+for /f "UseBackQ Delims=" %%R IN ("html_struct.txt") do (
+echo. %%R>>%Folder_name%\index.html
+echo. %Empty_str%>>%Folder_name%\index.html
+)
 Echo The project created!
+)
+pause
