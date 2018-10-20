ef24083b01bc7db3b098aa3e04a0f0c120879c3f
OlegUrmanov <olegurmanov@mail.ru>
2018-10-20 08:53:45 +0300

    Check input validation.

--- Task4/group-subscribe.js
+++ Task4/group-subscribe.js
@@ -1,6 +1,15 @@
 const myGroups = []; // массив для хранения подписок на группы
 
-const groupList = [{groupName: "Habrahabr", groupId: 0},{groupName: "Habrahabr", groupId: 1},{groupName: "Web2018", groupId: 2}];
+const groupList = [{
+  groupName: "Habrahabr",
+  groupId: 0
+}, {
+  groupName: "Habrahabr",
+  groupId: 1
+}, {
+  groupName: "Web2018",
+  groupId: 2
+}];
 
 /**
  * Функция подписки на группы
@@ -8,15 +17,18 @@ const groupList = [{groupName: "Habrahabr", groupId: 0},{groupName: "Habrahabr",
  */
 function subscribeGroup(groupId) {
   var keys = 0;
+  if (groupId !== 0 || groupId !== 1 || groupId !== 2){
+    console.log("can't subscribe on group ");
+  } else
   for (let key of groupList.values()) {
     if (myGroups.indexOf(groupId) == -1 && key.groupId == groupId) {
-      myGroups.push(key.groupName,groupId);
+      myGroups.push(key.groupName, groupId);
       console.log("you are just subscribe on " + key.groupName);
       keys = 1;
       return 0;
     }
-    }
-    if (keys == 0)
+  }
+  if (keys == 0)
     console.log("can't subscribe on group ");
 }
 
@@ -26,18 +38,18 @@ function subscribeGroup(groupId) {
  */
 function unsubscribeGroup(groupId) {
   for (let key of groupList.values()) {
-  if (myGroups.indexOf(groupId) != -1) {
-    myGroups.splice(myGroups.indexOf(groupId), 1);
-    myGroups.splice(myGroups.indexOf(key.groupName), 1);
-    console.log("you are just unsubscribe for " + groupId);
+    if (myGroups.indexOf(groupId) != -1) {
+      myGroups.splice(myGroups.indexOf(groupId), 1);
+      myGroups.splice(myGroups.indexOf(key.groupName), 1);
+      console.log("you are just unsubscribe for " + groupId);
+    }
   }
-}
-  if (groupId == "All" ){
-    for (let key of myGroups.values()){
-    myGroups.splice(myGroups.indexOf(key.groupId), 1);
-    myGroups.splice(myGroups.indexOf(key.groupName), 1);
-  }
-  console.log("you are just unsubscribe for All")
+  if (groupId == "All") {
+    for (let key of myGroups.values()) {
+      myGroups.splice(myGroups.indexOf(key.groupId), 1);
+      myGroups.splice(myGroups.indexOf(key.groupName), 1);
+    }
+    console.log("you are just unsubscribe for All")
   }
 }
 
